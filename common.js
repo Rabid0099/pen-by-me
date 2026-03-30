d3c13b8f397a45296049e62738e749a80787ac71
Pen By Me <penbyme@example.com>
2026-03-30 13:22:04 +0600

    Make site static for Netlify deployment - added common.js and updated products.html and Matador Claro.html

---
+++ common.js
@@ -0,0 +1,18 @@
+// Common products data for static deployment
+const products = [
+  { id: 1, name: 'Matador Claro', price: 9, image: 'product_pen_pic/Claro.jpg' },
+  { id: 2, name: 'Matador Smoothy Ballpen', price: 129.99, image: 'product_pen_pic/Matador Smoothy Ballpen.jpg' },
+  { id: 3, name: 'Matador All-time', price: 159.99, image: 'product_pen_pic/Matador All-time.jpg' },
+  { id: 4, name: 'Matador Envoy', price: 149.99, image: 'product_pen_pic/Matador Envoy.jpg' },
+  { id: 5, name: 'Matador Glory Crystal', price: 119.99, image: 'product_pen_pic/Matador Glory Crystal.png' },
+  { id: 6, name: 'Matador Gripper Gel', price: 109.99, image: 'product_pen_pic/Matador Gripper Gel.png' },
+  { id: 7, name: 'Matador Hi-School', price: 139.99, image: 'product_pen_pic/Matador Hi-School.jpg' },
+  { id: 8, name: 'Matador i-teen Rio', price: 124.99, image: 'product_pen_pic/Matador i-teen Rio.jpg' },
+  { id: 9, name: 'Matador i-teen', price: 114.99, image: 'product_pen_pic/Matador i-teen Aroma.jpg' },
+  { id: 10, name: 'Matador Orbit', price: 11.99, image: 'product_pen_pic/Matador Orbit.png' },
+  { id: 11, name: 'Matador Pencilic', price: 14.99, image: 'product_pen_pic/Matador Pencilic.png' },
+  { id: 12, name: 'Matador Pin-Point', price: 144.99, image: 'product_pen_pic/Matador Pin-Point.png' },
+  { id: 13, name: 'Matador Radiant', price: 154.99, image: 'product_pen_pic/Matador Radiant.png' },
+  { id: 14, name: 'Matador I-teen Bloom', price: 8.99, image: 'product_pen_pic/Matador Trio.png' },
+  { id: 15, name: 'Matador i-teen Grip', price: 179.99, image: 'product_pen_pic/Matador i-teen Aroma.jpg' }
+];
